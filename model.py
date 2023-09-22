7d1cea39c2b39fe5d1a30630e1dedb48be17899c
Ivan <43500385+lopharb@users.noreply.github.com>
2023-09-22 22:09:48 +0300

    Add files via upload

--- model.py
+++ model.py
@@ -1,3 +1,18 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torchvision.models import inception_v3, Inception_V3_Weights
+
+from custom import Identity
+# probably gonna use InceptionV3 as classifier
+
+
+# deleting the last FC layer (not sure about the dropout tho) since we need the cnn features
+classifier = inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1)
+classifier.dropout = Identity()
+classifier.fc = Identity()
+x = torch.rand(1, 3, 299, 299)
+classifier.eval()
+print(classifier(x).shape)
+
+# TODO add the detector and decoder
---
+++ training.py
@@ -0,0 +1,5 @@
+import model
+
+
+# TODO there will be the trainig loop
+# and currantly i have no idea how to train NLP networks :D

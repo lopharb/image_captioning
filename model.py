3e28c6da66e4aee176311e0100cc39de73e8ad2e
lopharb <vanekshkel@gmail.com>
2023-10-16 18:31:51 +0300

    completed the detector feature extraction

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.models import inception_v3, Inception_V3_Weights
 from torchvision import transforms
 from PIL import Image
@@ -16,19 +17,20 @@ class Decoder(nn.Module):
         pass
 
 
-convert_tensor = transforms.Compose(  # seems to be alright for both models
-    [transforms.Resize((1280, 1280)),
-     transforms.ToTensor()]
-)
-
-
 class attention_based_captioner(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-
+        self.transforms = {
+            'to_tensor_1280': transforms.Compose(  # seems to be alright for both models
+                [transforms.Resize((1280, 1280)),
+                 transforms.ToTensor()]
+            ),
+            'aumgents':
+            transforms.Compose(
+                [None]  # TODO think what i wanted to add there :D
+            )
+        }
         # initializing feature-exctracion models
-        # deleting the last FC layer (not sure about the dropout tho)
-        # since we need the cnn features
         self.cnn_extractor = inception_v3(
             weights=Inception_V3_Weights.IMAGENET1K_V1)
         self.cnn_extractor.dropout = Identity()
@@ -45,25 +47,30 @@ class attention_based_captioner(nn.Module):
 
         self.embedding = nn.Linear(2048, 256)  # eh?
 
-        self.decoder = None  # TODO add
+        self.decoder = Decoder()
 
     def forward(self, x: Image):
-        x_tens = convert_tensor(x)
+        """
+        this expects PIL Image as input
+        it then undergoes different conversions to match the networks requirements 
+        """
+
+        detections = self.detector(x)
+        tmp = detections.xywh[0].permute(1, 0)
+        importances = tmp[2]*tmp[3]*tmp[4]
+        concatted = torch.concat(
+            [detections.xywh[0], importances.view(importances.shape[0], -1)],
+            dim=1
+        ).flatten()
+        # concatted is: x_center, y_center, width, height, confidence, class_id, importance factor
+
+        x_tens = self.transforms['to_tensor_1280'](x)
         c, h, w = x_tens.shape  # h and w should be multiples of 32 so need to reshape
         cnn_features = self.cnn_extractor(x_tens.view(-1, c, h, w))
-
-        # for each image we need its width, height, confidence and class
+        # TODO need to work around the shape bc it doesn't seem to be right
+        print(cnn_features[0].shape)
         # (indexes 2-5 in .xywh)
-
-        detections = self.detector(x)  # TODO apply importance factor
-        tmp = detections.xywh[0].permute(
-            1, 0)  # TODO fix for batched input
-        importances = tmp[2]*tmp[3]*tmp[4]
-        print(detections.xywh[0].shape, importances.shape)
-        concatted = torch.concat(
-            [detections.xywh[0], importances.view(importances.shape[0], -1)], dim=1)
-        return concatted
-        # concatted is: x_center, y_center, width, height, confidence, class_id, importance factor
+        return F.pad(concatted, pad=(0, 2048-len(concatted))).view(1, 2048)
 
 
 if __name__ == '__main__':
@@ -74,4 +81,4 @@ if __name__ == '__main__':
     img = 'data/bicycle.jpg'
     img = transf(Image.open(img))
     res = model(img)
-    print(res)
+    print(res.shape)
